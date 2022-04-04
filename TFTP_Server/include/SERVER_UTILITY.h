890865355d34e0ab400f3f2c430b8417def5206b
Pratik Roy <roypratik.br@gmail.com>
2022-04-05 01:49:41 +0530

    Initial commit, file directories made

---
+++ TFTP_Server/include/SERVER_UTILITY.h
@@ -0,0 +1,35 @@
+/**********************************************************************************************
+ **  FILENAME	      : SERVER_UTILITY.h	
+ **:wq
+ **  DESCRIPTION      : This file defines macros and header files for source.c 
+ ** 
+ **
+ **  REVISION HISTORY :
+ **  
+ **  DATE	    NAME		   REFERENCE	    REASON
+ **  ------------------------------------------------------------------------------------
+ **  29 Mar 2022    Pratik Roy        	   Class test       Creation of the header file
+ **
+ **
+ **  Copyright @ 2022 Capgemini Engineering All Rights Reserved
+ **
+ ********************************************************************************************/
+
+/***************************************
+ * 		INCLUDES
+ **************************************/
+
+# include <stdio.h>
+# include <stdlib.h>
+
+/*******************************************************************
+ **  FUNCTION NAME	: myFunc
+ **
+ **  DESCRIPTION	: To make an executable command 
+ **
+ **  PARAMETERS		: Number of arguments, Double pointer to 
+ 			  argument variables
+ **
+ **  RETURN 		: 0 else exit with 1 
+ **
+ ******************************************************************/
